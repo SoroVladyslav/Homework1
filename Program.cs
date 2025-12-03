a3046cfe09e9e3186bd40008ca889c3e2e6a609d
SoroVladyslav <last.ash2020@gmail.com>
2025-12-03 18:37:48 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System.Globalization;
+
+//variables
+string name;
+string product;
+decimal price;
+string address;
+
+//order 1
+Console.Write("Enter client name: ");
+name = Console.ReadLine();
+
+Console.Write("Enter product: ");
+product = Console.ReadLine();
+
+Console.Write("Enter price: ");
+price = decimal.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+Console.Write("Enter address: ");
+address = Console.ReadLine();
+
+Console.WriteLine($"Order No 1");
+Console.WriteLine($"Client: {name}.");
+Console.WriteLine($"Product: {product}, Price: {price} EUR.");
+Console.WriteLine($"Adress: {address}.");
+
+//order 2
+Console.Write("Enter client name: ");
+name = Console.ReadLine();
+
+Console.Write("Enter product: ");
+product = Console.ReadLine();
+
+Console.Write("Enter price: ");
+price = decimal.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+Console.Write("Enter address: ");
+address = Console.ReadLine();
+
+Console.WriteLine($"Order No 2");
+Console.WriteLine($"Client: {name}.");
+Console.WriteLine($"Product: {product}, Price: {price} EUR.");
+Console.WriteLine($"Adress: {address}.");
+
+//order 3
+Console.Write("Enter client name: ");
+name = Console.ReadLine();
+
+Console.Write("Enter product: ");
+product = Console.ReadLine();
+
+Console.Write("Enter price: ");
+price = decimal.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+Console.Write("Enter address: ");
+address = Console.ReadLine();
+
+Console.WriteLine($"Order No 3");
+Console.WriteLine($"Client: {name}.");
+Console.WriteLine($"Product: {product}, Price: {price} EUR.");
+Console.WriteLine($"Adress: {address}.");
